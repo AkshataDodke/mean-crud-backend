4ac86df821599a3b703c72fb63a19057c9ca7c25
AkshataDodke <akshatadodke000@gmail.com>
2025-09-04 16:06:06 +0530

    added database name to the backend url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 // const MONGODB_URI =
 //   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mean_crud";
 const MONGODB_URI = process.env.MONGODB_URI;
